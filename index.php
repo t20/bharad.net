47539af302b264a06340b9bf7c816a96d13abc86
Bharad <barbi.bruce@gmail.com>
2012-07-09 23:27:49 -0700

    remove comments pop up in homepage

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 				<?php while (have_posts()) : the_post(); ?>
 					
 					<div class="blogpost">
-						<div class="comments"><?php comments_popup_link('0', '1', '%'); ?></div>
 						<h2 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 						<div class="meta">Posted <?php the_time('F jS, Y') ?> in <?php the_category(', ') ?> by <?php the_author() ?></div>
 						<div class="entry">
